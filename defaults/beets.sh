9a4c75bbf7a65443a6c9213e375a218f037a39fc
sparklyballs <sparklyballs@gmail.com>
2015-10-16 12:40:21 +0100

    fix checkstyle SC2086

--- defaults/beets.sh
+++ defaults/beets.sh
@@ -8,7 +8,7 @@
 # $1 - Fullpath of directory to be processed.  eg./mnt/user/downloads/some.artist_some.album
 
 # $7 - Status of post processing. 0 = OK, 1 = failed verification, 2 = failed unpack, 3 = 1+2
-if [ ! -z $7 ] && [ $7 -gt 0 ]; then
+if [ ! -z "$7" ] && [ "$7" -gt 0 ]; then
     echo "post-processing failed, bypassing script"
     exit 1
 fi
