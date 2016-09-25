0e1f228d6b8cc44bbafcb30a66b9f56c9ba685f1
sparklyballs <sparklyballs@gmail.com>
2016-09-25 09:02:39 +0100

    alpine rebase

--- init/40_set_config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-[[ ! -f /config/beets.sh ]] && cp /defaults/beets.sh /config/beets.sh
-[[ ! -f /config/config.yaml ]] && cp /defaults/config.yaml /config/config.yaml
-
-chown -R abc:abc /config
-
